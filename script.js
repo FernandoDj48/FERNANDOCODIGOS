687071b2cfc211cf80b3f8ea674ffb1244f73b56
FernandoDj48 <fernando.berkowitz@gmail.com>
2024-10-02 20:35:14 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 // Solicita um número ao usuário
-let numero = prompt("Digite um número:");
+let numero = prompt("Digite um número para ver a tabuada:");
 
 // Converte a entrada para número
-numero = parseFloat(numero);
+numero = parseInt(numero);
 
-// Verifica se o número é positivo, negativo ou zero
-if (numero > 0) {
-  alert("O número é positivo.");
-} else if (numero < 0) {
-  alert("O número é negativo.");
-} else {
-  alert("O número é zero.");
+// Exibe a tabuada do número
+for (let i = 1; i <= 10; i++) {
+  console.log(numero + " x " + i + " = " + (numero * i));
 }
